3a83ef188727791e70799c2a92cc33ce0593c2c4
Vitali Stsepaniuk <contact@vitaliy.dev>
2023-12-12 21:09:14 +0100

    refactor: day11

--- src/day11.rs
+++ src/day11.rs
@@ -1,10 +1,8 @@
+use aoc::Point2D;
 use itertools::Itertools;
 use pathfinding::matrix::Matrix;
 
 const INPUT: &str = include_str!("../input/day11.txt");
-
-#[derive(Debug, Copy, Clone)]
-struct Point(usize, usize);
 
 fn main() {
     println!("Part 1: {}", solve(INPUT, 1));
@@ -16,7 +14,8 @@ fn solve(input: impl AsRef<str>, gap_size: usize) -> usize {
 
     let galaxies = matrix
         .items()
-        .filter_map(|(pos, &c)| (c == '#').then(|| Point(pos.1, pos.0)))
+        .filter(|&(_, &c)| (c == '#'))
+        .map(|(pos, _)| Point2D::new(pos.1, pos.0))
         .collect::<Vec<_>>();
 
     let empty_rows = (0..matrix.rows)
@@ -31,10 +30,10 @@ fn solve(input: impl AsRef<str>, gap_size: usize) -> usize {
         .iter()
         .combinations(2)
         .map(|pair| {
-            manhattan_distance(pair[0], pair[1])
+            pair[0].manhattan(pair[1])
                 + gap_size
-                    * (gaps(pair[0].0, pair[1].0, &empty_cols)
-                        + gaps(pair[0].1, pair[1].1, &empty_rows))
+                    * (gaps(pair[0].x, pair[1].x, &empty_cols)
+                        + gaps(pair[0].y, pair[1].y, &empty_rows))
         })
         .sum()
 }
@@ -43,11 +42,6 @@ fn solve(input: impl AsRef<str>, gap_size: usize) -> usize {
 fn gaps(a: usize, b: usize, empty: &[usize]) -> usize {
     let (min, max) = (a.min(b), a.max(b));
     empty.iter().filter(|&&pos| min < pos && pos < max).count()
-}
-
-#[inline]
-fn manhattan_distance(a: &Point, b: &Point) -> usize {
-    (a.0 as isize - b.0 as isize).unsigned_abs() + (a.1 as isize - b.1 as isize).unsigned_abs()
 }
 
 #[cfg(test)]
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use derive_more::{Debug, Display};
+
 pub fn parse_nums<Output, Item>(input: impl AsRef<str>) -> Output
 where
     Output: FromIterator<Item>,
@@ -9,4 +11,33 @@ where
         .split(|c: char| c != '-' && (c.is_ascii_whitespace() || c.is_ascii_punctuation()))
         .filter_map(|n| n.parse().ok())
         .collect()
+}
+
+#[derive(Debug, Copy, Clone, Display)]
+#[display("({x},{y})")]
+#[debug("({x},{y})")]
+pub struct Point2D<T> {
+    pub x: T,
+    pub y: T,
+}
+
+impl<T> Point2D<T> {
+    pub fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T> Point2D<T>
+where
+    T: Copy + Ord + std::ops::Sub<Output = T> + std::ops::Add<Output = T>,
+{
+    pub fn manhattan(&self, other: &Self) -> T {
+        (self.x.max(other.x) - self.x.min(other.x)) + (self.y.max(other.y) - self.y.min(other.y))
+    }
+}
+
+impl<T> From<(T, T)> for Point2D<T> {
+    fn from((x, y): (T, T)) -> Self {
+        Self::new(x, y)
+    }
 }
