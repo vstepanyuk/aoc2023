2fe6d0968fef0465e6369ead7dcee7321583cdeb
Vitali Stsepaniuk <contact@vitaliy.dev>
2023-12-14 02:24:36 +0100

    chore: refactor day13

--- src/day13.rs
+++ src/day13.rs
@@ -1,28 +1,35 @@
-use itertools::Itertools;
 use num::Integer;
-use pathfinding::matrix::Matrix;
-
-use aoc::MatrixExt;
 
 const INPUT: &str = include_str!("../input/day13.txt");
 
-struct Frame(Matrix<char>);
+struct Frame {
+    rows: Vec<Vec<u8>>,
+    cols: Vec<Vec<u8>>,
+}
 
 impl Frame {
     fn new(input: impl AsRef<str>) -> Self {
-        Self(Matrix::from_iter(
-            input.as_ref().lines().map(|line| line.chars()),
-        ))
+        let rows = input
+            .as_ref()
+            .lines()
+            .map(|line| line.as_bytes().to_vec())
+            .collect::<Vec<_>>();
+
+        let cols = (0..rows[0].len())
+            .map(|col| rows.iter().map(|row| row[col]).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+
+        Self { rows, cols }
     }
 
-    fn find_mirror(&self) -> usize {
-        self.find_mirror_inner(self.0.rows_iter(), 100)
-            .or_else(|| self.find_mirror_inner(self.0.columns_iter(), 1))
+    fn mirror_value(&self) -> usize {
+        self.find(&self.rows, 0, None, 100)
+            .or_else(|| self.find(&self.cols, 0, None, 1))
             .unwrap_or_default()
     }
 
     #[inline]
-    fn diff<T: PartialEq>(a: &[T], b: &[T]) -> usize {
+    fn diff<'a, T: PartialEq>(a: &'a [T], b: &'a [T]) -> usize {
         a.iter().zip(b.iter()).filter(|(a, b)| a != b).count()
     }
 
@@ -31,58 +38,31 @@ impl Frame {
         s.len().is_even() && s.iter().zip(s.iter().rev()).all(|(a, b)| a == b)
     }
 
-    fn find_other_mirror(&self) -> usize {
-        self.find_mirror_other_inner(
-            self.0.rows_iter(),
-            self.find_mirror_inner(self.0.rows_iter(), 1)
-                .map(|idx| idx - 1),
-            100,
-        )
-        .or_else(|| {
-            self.find_mirror_other_inner(
-                self.0.columns_iter(),
-                self.find_mirror_inner(self.0.columns_iter(), 1)
-                    .map(|idx| idx - 1),
-                1,
-            )
-        })
-        .unwrap_or_default()
-    }
+    fn mirror_value_with_smudge(&self) -> usize {
+        let split_idx = self.find(&self.rows, 0, None, 1).map(|idx| idx - 1);
 
-    fn find_mirror_inner<'a, T: 'a + PartialEq>(
-        &'a self,
-        items: impl Iterator<Item = Vec<&'a T>>,
-        factor: usize,
-    ) -> Option<usize> {
-        let items = items.collect::<Vec<_>>();
+        self.find(&self.rows, 1, split_idx, 100)
+            .or_else(|| {
+                let split_idx = self.find(&self.cols, 0, None, 1).map(|idx| idx - 1);
 
-        items
-            .iter()
-            .tuple_windows()
-            .enumerate()
-            .find(|(pos, (a, b))| {
-                a == b
-                    && items[*pos + 2..]
-                        .iter()
-                        .zip(items[..*pos].iter().rev())
-                        .all(|(a, b)| a == b)
+                self.find(&self.cols, 1, split_idx, 1)
             })
-            .map(|(idx, _)| (idx + 1) * factor)
+            .unwrap_or_default()
     }
 
-    fn find_mirror_other_inner<'a, T: 'a + PartialEq>(
+    fn find<'a, T: 'a + PartialEq>(
         &'a self,
-        items: impl Iterator<Item = Vec<&'a T>>,
+        items: &[Vec<T>],
+        allowed_diff: usize,
         ignore: Option<usize>,
         factor: usize,
     ) -> Option<usize> {
-        let items = items.collect::<Vec<_>>();
         let items_len = items.len();
 
         (0..items_len)
             .flat_map(|i| (i + 1..items_len).map(move |j| (i, j)))
             .filter(|&(i, j)| {
-                Self::diff(&items[i], &items[j]) == 1
+                Self::diff(&items[i], &items[j]) == allowed_diff
                     && Self::is_reflective(&items[i + 1..j])
                     && Some((j + i) / 2) != ignore
             })
@@ -118,8 +98,8 @@ where
 }
 
 fn main() {
-    println!("Part 1: {}", solve(INPUT, Frame::find_mirror));
-    println!("Part 2: {}", solve(INPUT, Frame::find_other_mirror));
+    println!("Part 1: {}", solve(INPUT, Frame::mirror_value));
+    println!("Part 2: {}", solve(INPUT, Frame::mirror_value_with_smudge));
 }
 
 #[cfg(test)]
@@ -167,14 +147,14 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(solve(INPUT, Frame::find_mirror), 405);
-        assert_eq!(solve(INPUT2, Frame::find_mirror), 3)
+        assert_eq!(solve(INPUT, Frame::mirror_value), 405);
+        assert_eq!(solve(INPUT2, Frame::mirror_value), 3)
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(solve(INPUT, Frame::find_other_mirror), 400);
-        assert_eq!(solve(INPUT2, Frame::find_other_mirror), 1000);
-        assert_eq!(solve(INPUT3, Frame::find_other_mirror), 5);
+        assert_eq!(solve(INPUT, Frame::mirror_value_with_smudge), 400);
+        assert_eq!(solve(INPUT2, Frame::mirror_value_with_smudge), 1000);
+        assert_eq!(solve(INPUT3, Frame::mirror_value_with_smudge), 5);
     }
 }
